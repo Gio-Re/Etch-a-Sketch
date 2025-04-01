ae5b629d609b122ded9c654c5e771c5c7130bf7f
Gio-Re <181334102+Gio-Re@users.noreply.github.com>
2025-04-01 22:58:37 +0200

    Change color of cell when mouse pressed
    check mouse pressed and conditional func in event listener of cells

--- script.js
+++ script.js
@@ -1,29 +1,45 @@
-const container = document.querySelector('#container');
+const container = document.querySelector("#container");
 
 let numberOfSideCell = 16;
 let numberOfCell = numberOfSideCell * numberOfSideCell;
 for (let i = 0; i < numberOfSideCell; i++) {
-    const row = document.createElement('div');
-    row.classList.add('row');
-    container.appendChild(row);
-};
+  const row = document.createElement("div");
+  row.classList.add("row");
+  container.appendChild(row);
+}
 
-const rows = document.querySelectorAll('.row')
+const rows = document.querySelectorAll(".row");
 
 rows.forEach((row) => {
-    for (let j = 0; j < numberOfSideCell; j++) {
-        const cell = document.createElement('div');
-        cell.classList.add('cell');
-        row.appendChild(cell);
-    };
+  for (let j = 0; j < numberOfSideCell; j++) {
+    const cell = document.createElement("div");
+    cell.classList.add("cell");
+    row.appendChild(cell);
+  }
 });
 
-const cells = document.querySelectorAll('.cell');
+const cells = document.querySelectorAll(".cell");
 
+let mousePressed = false;
+document.addEventListener("mousedown", () => {
+  mousePressed = true;
+});
+document.addEventListener("mouseup", () => {
+  mousePressed = true;
+});
 
 cells.forEach((cell) => {
-    cell.addEventListener('mouseover', (e) => {
-        console.log(e.target);
-        e.target.setAttribute("style","background-color: black;");
-    });
+  cell.addEventListener("mouseover", (e) => {
+    if (mousePressed) {
+      e.target.setAttribute("style", "background-color: black;");
+    };
+  });
 });
+
+/* cells.forEach((cell) => {
+  cell.addEventListener("mouseleave", (e) => {
+    if(e.button != 1) {
+    e.target.setAttribute("style", "background-color: white;");
+    }
+  });
+}); */
