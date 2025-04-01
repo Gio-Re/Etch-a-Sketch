3f019489a804476cd789fcf91571216875b40165
Gio-Re <181334102+Gio-Re@users.noreply.github.com>
2025-04-01 19:54:28 +0200

    Create Rows and Column of the grid
    Throw JS create divs row that contein the cell. To get the cell fit in the div conteiner the
    conteiner css display is a flex box column, then the row are flexbox direction row.

--- script.js
+++ script.js
@@ -2,8 +2,18 @@ const container = document.querySelector('#container');
 
 let numberOfSideCell = 16;
 let numberOfCell = numberOfSideCell * numberOfSideCell;
-for (let i = 0; i < numberOfCell; i++) {
-    const cell = document.createElement('div');
-    cell.classList.add('cell');
-    container.appendChild(cell);
+for (let i = 0; i < numberOfSideCell; i++) {
+    const row = document.createElement('div');
+    row.classList.add('row');
+    container.appendChild(row);
 };
+
+const rows = document.querySelectorAll('.row')
+
+rows.forEach((row) => {
+    for (let j = 0; j < numberOfSideCell; j++) {
+        const cell = document.createElement('div');
+        cell.classList.add('cell');
+        row.appendChild(cell);
+    };
+})
