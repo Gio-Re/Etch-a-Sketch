75bcabeeb2aaea127979f9353b80279d39e87e7d
Gio-Re <181334102+Gio-Re@users.noreply.github.com>
2025-04-02 18:08:38 +0200

    Add function to draw shadow
    The function change css opacity, if colled replace cell with cloned version

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 //check if mouse is pressed
 const container = document.querySelector("#container");
 let mousePressed = false;
-container.addEventListener("mousedown", () => {
+document.addEventListener("mousedown", () => {
   mousePressed = true;
 });
-container.addEventListener("mouseup", () => {
+document.addEventListener("mouseup", () => {
   mousePressed = false;
 });
 
@@ -44,7 +44,7 @@ function resizeGrid() {
         const rows = document.querySelectorAll(".row");
         rows.forEach((row) => {
             container.removeChild(row);
-        })
+        });
         CreateGrid(sizeRequested);
         cellsToBlack();
     };
@@ -54,43 +54,67 @@ function resizeGrid() {
 function resetCells() {
   const cells = document.querySelectorAll(".cell");
   cells.forEach((cell) => {
-    cell.setAttribute("style", "background-color: white;")
+    cell.setAttribute("style", "background-color: white; opacity: 0.10;")
   });
 };
 
 //function to black cells
 function cellsToBlack () {
+  resetCells();
   const cells = document.querySelectorAll(".cell");
   cells.forEach((cell) => {
-    cell.addEventListener("mouseover", (e) => {
+    cell.addEventListener("mouseover", () => {
       if (mousePressed) {
-        e.target.setAttribute("style", "background-color: black;");
+        cell.setAttribute("style", "background-color: black; opacity: 1;");
       };
     });
   });
 };
 
 //function to Random color cells
-function randomColorToCells() {
+function cellsToRandomColor() {
+  resetCells();
   const cells = document.querySelectorAll(".cell");
   cells.forEach((cell) => {
     const r = Math.floor(Math.random() * 256);
     const g = Math.floor(Math.random() * 256);
     const b = Math.floor(Math.random() * 256);  
-    cell.addEventListener("mouseover", (e) => {
+    cell.addEventListener("mouseover", () => {
       if (mousePressed) {
-        e.target.setAttribute("style", "background-color: rgb(" + r + "," + g + "," + b + ");");
+        cell.setAttribute("style", "background-color: rgb(" + r + "," + g + "," + b + "); opacity: 1;");
       };
     });
   });
+};
 
-}
+//function to black shadows cells
+function cellsToShadow() {
+  resetCells();
+  const cells = document.querySelectorAll(".cell");
+  cells.forEach((cell) => {
+    cell.replaceWith(cell.cloneNode(false));
+  });
+  const newcells = document.querySelectorAll(".cell");
+  newcells.forEach((cell) => {
+    cell.addEventListener("mouseover", () => {
+      if (mousePressed) {
+        let currentOpacity = parseFloat(cell.style.opacity) || 0;
+        let desiredOpacity = 0;
+        if (currentOpacity < 1) {
+          desiredOpacity = currentOpacity + 0.1;
+          cell.setAttribute("style", "background-color: black; opacity: " + desiredOpacity + ";");
+        };
+        
+      };
+    });
+  });
+};
 
 //Call functions to create grid and to default skectch
 const defaultSize = 16;
-let mode;
 CreateGrid(defaultSize);
-cellsToBlack();
+cellsToShadow();
+
 
 //Button Resize
 const resize = document.querySelector('#resize');
@@ -102,13 +126,13 @@ reset.addEventListener('click', resetCells);
 
 //Button Black
 const black = document.querySelector('#black');
-black.addEventListener('click', () => {
-  mode = "black";
-});
+black.addEventListener('click', cellsToBlack);
 
 //Button Random
 const randomColor = document.querySelector('#randomColor');
-randomColor.addEventListener('click', () => {
-  mode = "randomColor";
-});
+randomColor.addEventListener('click', cellsToRandomColor);
+
+//Button Shadow
+const shadow = document.querySelector('#shadow');
+shadow.addEventListener('click', cellsToShadow);
 
