50caab070bb4ba3684c9c18e191faa3966675d36
Gio-Re <181334102+Gio-Re@users.noreply.github.com>
2025-04-02 14:07:09 +0200

    Add function to reset the sketch on the grid
    Add a listener to the reset button to reset throught the function the grid

--- script.js
+++ script.js
@@ -1,5 +1,39 @@
+//check if mouse is pressed
 const container = document.querySelector("#container");
-let numberOfSideCell = 16;
+let mousePressed = false;
+container.addEventListener("mousedown", () => {
+  mousePressed = true;
+});
+container.addEventListener("mouseup", () => {
+  mousePressed = false;
+});
+
+//function to create row of grid
+function CreateRows(size) {
+  for (let i = 0; i < size; i++) {
+    const row = document.createElement("div");
+    row.classList.add("row");
+    container.appendChild(row);
+  };
+};
+
+//function to create cells in rows
+function CreateCells(size){
+  const rows = document.querySelectorAll(".row");
+  rows.forEach((row) => {
+    for (let j = 0; j < size; j++) {
+      const cell = document.createElement("div");
+      cell.classList.add("cell");
+      row.appendChild(cell);
+    };
+  });
+};
+
+//function to create the grid
+function CreateGrid(size) {
+  CreateRows(size);
+  CreateCells(size);
+};
 
 //function to resize grid
 function resizeGrid() {
@@ -16,60 +50,37 @@ function resizeGrid() {
     };
 };
 
-//function to black cells
-function cellsToBlack () {
+//function to reset cell
+function resetCells() {
   const cells = document.querySelectorAll(".cell");
-        cells.forEach((cell) => {
-          cell.addEventListener("mouseover", (e) => {
-            if (mousePressed) {
-              e.target.setAttribute("style", "background-color: black;");
-            };
-          });
-        });
-}
-
-
-//function to create row of grid
-function CreateRows(numberOfSideCell) {
-  for (let i = 0; i < numberOfSideCell; i++) {
-    const row = document.createElement("div");
-    row.classList.add("row");
-    container.appendChild(row);
-  };
-};
-
-//function to create cells in rows
-function CreateCells(numberOfSideCell){
-  const rows = document.querySelectorAll(".row");
-  rows.forEach((row) => {
-    for (let j = 0; j < numberOfSideCell; j++) {
-      const cell = document.createElement("div");
-      cell.classList.add("cell");
-      row.appendChild(cell);
-    };
+  cells.forEach((cell) => {
+    cell.setAttribute("style", "background-color: white;")
   });
 };
 
-//function to create the grid
-function CreateGrid(numberOfSideCell) {
-  CreateRows(numberOfSideCell);
-  CreateCells(numberOfSideCell);
+//function to black cells
+function cellsToBlack () {
+  const cells = document.querySelectorAll(".cell");
+  cells.forEach((cell) => {
+    cell.addEventListener("mouseover", (e) => {
+      if (mousePressed) {
+        e.target.setAttribute("style", "background-color: black;");
+      };
+    });
+  });
 };
 
 //Call functions to create grid and to default skectch
-CreateGrid(numberOfSideCell);
+let defaultSize = 16;
+CreateGrid(defaultSize);
 cellsToBlack();
 
-//Button resize
+//Button Resize
 const resize = document.querySelector('#resize');
 resize.addEventListener('click', resizeGrid);
 
-//check if mouse is pressed
-let mousePressed = false;
-container.addEventListener("mousedown", () => {
-  mousePressed = true;
-});
-container.addEventListener("mouseup", () => {
-  mousePressed = false;
-});
+//Button Reset
+const reset = document.querySelector("#reset");
+reset.addEventListener('click', resetCells);
+
 
